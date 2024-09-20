51ffd8eff80ecd149235e52abf8d77a7e6a674ab
Dennis Rocha <dennis0201@gmail.com>
2024-09-20 20:51:32 -0300

    Ajustes no código

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from myapp.models import Colaborador
 
 
 
@@ -8,24 +8,19 @@ def frente(request):
     return render(request, 'myapp/globals/frente.html')
 
 def cadastrar(request):
-    
-    
     if request.method == 'POST':
         nome = request.POST.get('nome')
         cpf = request.POST.get('cpf')
         cargo = request.POST.get('cargo')
         telefone = request.POST.get('telefone')
-        print(nome, cpf, cargo, telefone)
         if nome and cpf and cargo and telefone:
-            colaboradores.objects.create(
+            Colaborador.objects.create(
                 nome=nome,
                 cpf=cpf,
                 cargo=cargo,
                 telefone=telefone
-                
             )
-            print(nome, cpf, cargo, telefone)
-            return redirect('colaboradores')
+            return redirect(colaboradores)
 
     
     return render(request, 'myapp/globals/cadastrar.html') 
@@ -34,5 +29,5 @@ def atualizar(request):
     return render(request, 'atualizar.html')
 
 def colaboradores(request):
-    colaboradores[0] = {"nome": "Joaquim","cpf": "543.321.156.23","cargo": "servente","telefone": "(49) 91359-1461"}
-    return render(request, 'myapp/globals/colaboradores.html', {"colaboradores":colaboradores})
+    colaborador = Colaborador.objects.all()
+    return render(request, 'myapp/globals/colaboradores.html', {"colaboradores":colaborador})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.urls import path
 from myapp import views
-from myapp.views import frente, cadastrar, atualizar, colaboradores
 
 urlpatterns = [
     path('', views.frente),
